f48dd3daf5e197c227c461d4c3ff57872fd48ce7
Marcus Dias <mvrdias@uol.com.br>
2017-10-06 18:53:40 +0000

    return

---
+++ my-module.js
@@ -0,0 +1,10 @@
+// Exercicio W1D5 - Modulos 01 //
+function mult (numb) {
+  return numb *100;
+}
+
+function div (numb) {
+  return numb / 100;
+}
+
+module.exports = { multiplicar: mult, dividir: div };
---
+++ my-modulecall.js
@@ -0,0 +1,2 @@
+var chamada = require('./my-module');
+console.log (chamada.multiplicar(9), chamada.dividir(9));
